1210077db9d4b9326e0409429b71ac0831bb526e
Divyesh Ladva <ladoodivyesh@gmail.com>
2025-06-09 07:13:41 +0000

    feat(product-service): enable emitting ProductCreated event for analytics-service

--- apps/product-service/src/app.module.ts
+++ apps/product-service/src/app.module.ts
@@ -3,6 +3,7 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { Product, ProductSchema } from './schemas/product';
+import { ClientsModule, Transport } from '@nestjs/microservices';
 
 @Module({
   imports: [
@@ -12,7 +13,20 @@ import { Product, ProductSchema } from './schemas/product';
         name: Product.name,
         schema: ProductSchema
       }
-    ])
+    ]),
+    ClientsModule.register([
+      {
+        name: 'ANALYTICS_QUEUE_CLIENT',
+        transport: Transport.RMQ,
+        options: {
+          urls: ['amqp://dvs:dvs@172.20.240.1:5672/nestjs_vhost'],
+          queue: 'analytics_queue',
+          queueOptions: {
+            durable: true
+          },
+        },
+      },
+    ]),
   ],
   controllers: [AppController],
   providers: [AppService],
--- apps/product-service/src/app.service.ts
+++ apps/product-service/src/app.service.ts
@@ -1,13 +1,17 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { ProductCreateRequest } from '@app/protos/product'
 import { Product } from './schemas/product';
 import { Model } from 'mongoose';
+import { ClientRMQ } from '@nestjs/microservices';
 
 @Injectable()
 export class AppService {
   
-  constructor(@InjectModel(Product.name) private productModel: Model<Product>) {}
+  constructor(
+    @InjectModel(Product.name) private productModel: Model<Product>,
+    @Inject('ANALYTICS_QUEUE_CLIENT') private analyticsQueueClient: ClientRMQ
+  ) {}
   
   public async create(data: ProductCreateRequest): Promise<boolean> {
     const product = new this.productModel({
@@ -15,8 +19,9 @@ export class AppService {
       title: data.title
     })
     
+    let result = {};
     try {
-      const result = await product.save();
+      result = await product.save();
       console.log(JSON.parse(JSON.stringify(result)))
     } catch(error: unknown) {
       console.error(error);
@@ -26,6 +31,12 @@ export class AppService {
         return false;
       }
     }
+    
+    const pattern = 'ProductCreated';
+    this.analyticsQueueClient.send(pattern, result).subscribe({
+      complete: () => console.log(`Event "${pattern}" sent successfully to RabbitMQ.`),
+      error: (err) => console.error(`Failed to send event "${pattern}" to RabbitMQ:`, err),
+    })
     
     return true;
   }
